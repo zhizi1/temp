d9e592e85e452084c3a2a6cb6ce3747bf05e03b7
zhizi <zhizi@kdd.com>
2019-04-18 19:51:14 +0800

    first commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,12 +19,11 @@ module.exports = {
 
   deploy : {
     production : {
-	    key: '~/.ssh',    
       user : 'root',
       host : '123.207.173.87',
       ref  : 'origin/master',
       repo : 'git@github.com:zhizi1/temp.git',
-      path : '/root/www/production',
+      path : '/root/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(1111)
+const http = require('http')
+const app = http.createServer((req, res) => {
+res.end('hello, zhizi')
+})
+
+app.listen('80', () => {
+
+console.log('the serve is running 80 port')
+})
