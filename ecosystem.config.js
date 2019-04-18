6d721991342da5a0439428d750ec6a1d5eba6b71
zhizi <zhizi@kdd.com>
2019-04-18 19:26:34 +0800

    temp commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       host : '123.207.173.87',
       ref  : 'origin/master',
       repo : 'git@github.com:zhizi1/temp.git',
-      path : '/var/www/production',
+      path : '/root/www/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
